edb135bc4a4cc954ff0b6a1016f9a8e11118f3b9
Vitalii-t9 <vitalik2014titov@gmail.com>
2021-08-23 20:30:27 +0300

    added functions for clicking, selecting and send text

--- scripts/Tests/mapHomePage.py
+++ scripts/Tests/mapHomePage.py
@@ -3,8 +3,15 @@ import unittest
 import sys
 import os
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from scripts.Pages.homePage import HomePage
+from scripts.Pages.cartPage import CartPage
+from scripts.Pages.accountPage import AccountPage
+from scripts.Pages.contactUsPage import ContactUs
+from scripts.Pages.createAnAccountPage import CreateAnAccountPage
+from scripts.Pages.signInPage import SignInPage
+from scripts.Pages.signOutPage import SignOutPage
 import HtmlTestRunner
 
 class MapHomePage(unittest.TestCase):
@@ -15,15 +22,22 @@ class MapHomePage(unittest.TestCase):
         cls.driver.implicitly_wait(10)
         # cls.driver.maximize_window()
 
+
     def test_links_on_hp(self):
         driver = self.driver
         driver.get('http://automationpractice.com/index.php')
 
         home = HomePage(driver)
-        home.search("dresses")
-        time.sleep(6)
-        home.click_signIn()
-        time.sleep(5)
+        contactUs = ContactUs(driver)
+        home.click_on_locator(home.contactUs_link_textlink)
+        contactUs.select_by_visibletext(contactUs.subjectHeading_dropdownmenu_id, contactUs.subjectHeading_dropdownmenuitem_visibletext)
+        time.sleep(3)
+        contactUs.enter_text(contactUs.email_textbox_id, "any@gmail.com")
+        time.sleep(2)
+        contactUs.enter_text(contactUs.orderReference_textbox_id, "Any text")
+        time.sleep(2)
+        contactUs.enter_text(contactUs.message_textbox_id, "Any text two")
+        time.sleep(2)
 
     @classmethod
     def tearDownClass(cls):
